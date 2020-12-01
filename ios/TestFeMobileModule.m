ededf2d7b28267bbd4d8cab888e88564f0ebae17
Pavel Belinovich <pbelinovich@jtconsulting.ru>
2020-12-01 12:47:15 +0300

    Developed the first test native module (IOS)

--- ios/TestFeMobileModule.m
+++ ios/TestFeMobileModule.m
@@ -2,8 +2,14 @@
 
 @interface RCT_EXTERN_MODULE(TestFeMobileModule, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
+RCT_EXTERN_METHOD(sampleMethodWithCallback:(NSString *)stringArgument secondArgument:(NSInteger *)numberArgument withCallback:(RCTResponseSenderBlock)callback)
+
+RCT_EXTERN_METHOD(sampleMethodWithPromise:(NSString *)stringArgument secondArgument:(NSInteger *)numberArgument
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(showText:(NSString *)message secondArgument:(NSInteger *)duration)
+
+RCT_EXTERN_METHOD(getTextLength:(NSString *)message withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
 
 @end
